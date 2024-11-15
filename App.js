fc2b408fba747f389239782b242e251e00a591aa
Mieulchi <sangsangujk@hansung.ac.kr>
2024-11-16 00:03:04 +0900

    Project Init

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function Home() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Home!</Text>
+		</View>
+	);
 }
 
+function RunningHome() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>RunnikngHome!</Text>
+		</View>
+	);
+}
+
+function MyRunning() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>My Running!</Text>
+		</View>
+	);
+}
+
+function MyPage() {
+	return (
+		<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+			<Text>Settings!</Text>
+		</View>
+	);
+}
+
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="Home" component={Home} />
+				<Tab.Screen name="RunningHome" component={RunningHome} />
+				<Tab.Screen name="MyRunning" component={MyRunning} />
+				<Tab.Screen name="MyPage" component={MyPage} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
+}
+
+const Tab = createBottomTabNavigator();
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
